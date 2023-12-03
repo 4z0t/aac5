7615087e896d6be368d79133041455d70f0bdc6d
4z0t <gastec139@gmail.com>
2023-12-03 16:26:19 +0300

    Добавьте файлы проекта.

---
+++ aac5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = File.ReadAllText("../../../Resources/Code.txt");
+            Interpreter interpreter = new Interpreter();
+            interpreter.Interpret(code);
+        }
+    }
+}
