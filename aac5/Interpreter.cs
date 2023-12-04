57a469daa388a6228566303caf78b6fd31fc5091
4z0t <gastec139@gmail.com>
2023-12-04 16:55:48 +0300

    split on classes

--- aac5/Interpreter.cs
+++ aac5/Interpreter.cs
@@ -7,6 +7,9 @@ using static Interpreter.Tokenizer;
 
 namespace Interpreter
 {
+
+    class BreakException : Exception { };
+    class ContinueException : Exception { };
     public class Interpreter
     {
         public void Interpret(string code)
@@ -18,9 +21,16 @@ namespace Interpreter
             Interpret(context);
         }
 
-
-        class BreakException : Exception { };
-        class ContinueException : Exception { };
+        private static readonly Dictionary<string, IStatement> statements = new()
+        {
+            ["if"] = new IfStatement(),
+            ["for"] = new ForStatement(),
+            ["while"] = new WhileStatement(),
+            ["print"] = new PrintStatement(),
+            ["scan"] = new ScanStatement(),
+            ["continue"] = new ContinueStatement(),
+            ["break"] = new BreakStatement(),
+        };
 
         private void Interpret(Context context)
         {
@@ -29,11 +39,11 @@ namespace Interpreter
                 while (context.Tokens.Count > 0)
                     Statement(context);
             }
-            catch(BreakException)
+            catch (BreakException)
             {
                 Console.WriteLine("Unexpected break statement");
             }
-            catch(ContinueException)
+            catch (ContinueException)
             {
                 Console.WriteLine("Unexpected continue statement");
             }
@@ -50,54 +60,20 @@ namespace Interpreter
             var tokenFull = context.Tokens.Pop();
             var type = tokenFull.Type;
             var token = tokenFull.TokenString;
-            if (token == "if")
+            if (statements.ContainsKey(token))
             {
-                EvalIfStatement(context);
-            }
-            else if (token == "for")
-            {
-                ForStatement(context);
-            }
-            else if (token == "while")
-            {
-                WhileStatement(context);
-            }
-            else if (token == "print")
-            {
-                Print(context);
-            }
-            else if (token == "scan")
-            {
-                Scan(context);
-            }
-            else if (token == "continue")
-            {
-                Continue(context);
-            }
-            else if (token == "break")
-            {
-                Break(context);
+                statements[token].Process(this, context);
             }
             else if (type == TokenType.Character)
             {
                 context.Tokens.Push(tokenFull);
-                Assign(context);
+                AssignStatement(context);
             }
             else
                 throw ExpressionsHellper.ThrowUnexpectedToken(tokenFull);
         }
 
-
-        private void Break(Context context)
-        {
-            throw new BreakException();
-        }
-        private void Continue(Context context)
-        {
-            throw new ContinueException();
-        }
-
-        private void Assign(Context context)
+        private void AssignStatement(Context context)
         {
             ExpressionsHellper.CheckStack(context);
             var identifier = context.Tokens.Pop();
@@ -108,178 +84,7 @@ namespace Interpreter
             context.Variables[identifier.TokenString] = expression;
         }
 
-        private void Scan(Context context)
-        {
-            ExpressionsHellper.CheckStack(context);
-            var token = context.Tokens.Pop();
-            if (token.Type != TokenType.Character)
-                throw new Exception($"Invalid symbol for \"scan\" {token.TokenString}.");
-            var value = Int32.Parse(Console.ReadLine());
-            context.Variables[token.TokenString] = value;
-            ExpressionsHellper.CheckStack(context);
-            token = context.Tokens.Pop();
-            if (token.TokenString != ";")
-                throw new Exception($"Invalid symbol for \"scan\" {token.TokenString}.");
-        }
-
-        private void Print(Context context)
-        {
-            var result = PrintEnd(context);
-            Console.WriteLine(result);
-        }
-
-        private string PrintEnd(Context context)
-        {
-            var stack = context.Tokens;
-            ExpressionsHellper.CheckStack(context);
-            var token = stack.Pop();
-            if (token.Type == TokenType.QuoteString)
-            {
-                if (stack.Count > 0 && stack.Peek().TokenString == ",")
-                {
-                    stack.Pop();
-                    return token.TokenString + " " + PrintEnd(context);
-                }
-                else
-                    return token.TokenString;
-            }
-            else if (token.Type == TokenType.Character || token.Type == TokenType.Digit)
-            {
-                context.Tokens.Push(token);
-                var expression = Expression.Parse(context).ToString();
-                bool isNextComma = stack.Count > 0 && stack.Peek().TokenString == ",";
-                if (stack.Count > 0 && isNextComma)
-                {
-                    stack.Pop();
-                    return expression + " " + PrintEnd(context);
-                }
-                else
-                    return expression;
-            }
-            else
-                throw new Exception("Invalid <print_end>.");
-        }
-
-        private void WhileStatement(Context context)
-        {
-            ExpressionsHellper.CheckStack(context);
-            var stack = context.Tokens;
-            var boolExprCommands = new List<Token>();
-            var token = stack.Pop();
-            while (token.TokenString != "{")
-            {
-                boolExprCommands.Add(token);
-                token = stack.Pop();
-            }
-
-            int bracketCount = 1;
-            var commands = new List<Token>
-            {
-                token
-            };
-
-            while (stack.Count > 0 && bracketCount != 0)
-            {
-                token = stack.Pop();
-                commands.Add(token);
-                if (token.TokenString == "{")
-                    bracketCount++;
-                if (token.TokenString == "}")
-                    bracketCount--;
-            }
-            if (bracketCount > 0)
-                throw new Exception("Expected '}'");
-
-
-            while (true)
-            {
-                var exprContext = new Context(boolExprCommands, context.Variables);
-                if (EvalBoolExpression(exprContext))
-                {
-                    try
-                    {
-                        var forContext = new Context(commands, context.Variables);
-                        EvalStatement(forContext);
-                    }
-                    catch (ContinueException)
-                    {
-                        continue;
-                    }
-                    catch (BreakException)
-                    {
-                        break;
-                    }
-                    catch { throw; }
-                }
-                else
-                    break;
-            }
-        }
-
-        private void ForStatement(Context context)
-        {
-            ExpressionsHellper.CheckStack(context);
-            var stack = context.Tokens;
-            var charecter = stack.Pop();
-            if (charecter.Type != TokenType.Character)
-                throw ExpressionsHellper.ThrowUnexpectedToken(charecter);
-            ExpressionsHellper.CheckStack(context);
-            var equal = stack.Pop();
-            if (equal.Type != TokenType.Equal)
-                throw ExpressionsHellper.ThrowUnexpectedToken(equal);
-            var left = Expression.Parse(context);
-            ExpressionsHellper.CheckStack(context);
-            var to = stack.Pop();
-            if (to.TokenString != "to")
-                throw ExpressionsHellper.ThrowUnexpectedToken(equal);
-            var right = Expression.Parse(context);
-            context.Variables[charecter.TokenString] = left;
-            EvalForLoop(context, charecter, right);
-        }
-
-        private void EvalForLoop(Context context, Token charecter, int right)
-        {
-            var queue = context.Tokens;
-            var commands = new List<Token>();
-            int bracketCount = 1;
-            var token = queue.Pop();
-            if (token.TokenString != "{")
-                throw ExpressionsHellper.ThrowUnexpectedToken(token);
-            commands.Add(token);
-            while (queue.Count > 0 && bracketCount != 0)
-            {
-                token = queue.Pop();
-                commands.Add(token);
-                if (token.TokenString == "{")
-                    bracketCount++;
-                if (token.TokenString == "}")
-                    bracketCount--;
-            }
-            if (bracketCount > 0)
-                throw new Exception("Expected '}'");
-
-            while (context.Variables[charecter.TokenString] < right)
-            {
-                try
-                {
-                    var forContext = new Context(commands, context.Variables);
-                    EvalStatement(forContext);
-                    context.Variables[charecter.TokenString]++;
-                }
-                catch (ContinueException)
-                {
-                    context.Variables[charecter.TokenString]++;
-                    continue;
-                }
-                catch (BreakException)
-                {
-                    break;
-                }
-            }
-        }
-
-
-        private void SkipStatement(Context context)
+        public void SkipStatement(Context context)
         {
             var queue = context.Tokens;
             var token = queue.Pop();
@@ -298,36 +103,17 @@ namespace Interpreter
                 throw new Exception("Expected '}'.");
         }
 
-        private void EvalIfStatement(Context context)
-        {
-            var queue = context.Tokens;
-            if (EvalBoolExpression(context))
-            {
-                EvalStatement(context);
-                if (queue.Count != 0 && queue.Peek().TokenString == "else")
-                {
-                    queue.Pop();
-                    SkipStatement(context);
-                }
-            }
-            else
-            {
-                SkipStatement(context);
-                ElseBlock(context);
-            }
-        }
-
         private static void PrintStack(Stack<Token> stack)
         {
             var stackCopy = new Stack<Token>(stack.Reverse());
-            while(stackCopy.Count != 0)
+            while (stackCopy.Count != 0)
             {
                 Console.WriteLine(stackCopy.Pop());
             }
             Console.WriteLine();
         }
 
-        private void EvalStatement(Context context)
+        public void EvalStatement(Context context)
         {
             var queue = context.Tokens;
             ExpressionsHellper.CheckStack(context);
@@ -342,47 +128,6 @@ namespace Interpreter
             token = queue.Pop();
             if (token.TokenString != "}")
                 ExpressionsHellper.ThrowUnexpectedToken(token);
-        }
-
-        private void ElseBlock(Context context)
-        {
-            var queue = context.Tokens;
-            if (queue.Count == 0)
-                return;
-            var token = queue.Peek();
-            if (token.TokenString == "else")
-            {
-                queue.Pop();
-                EvalStatement(context);
-            }
-        }
-
-        private bool EvalBoolExpression(Context context)
-        {
-            ExpressionsHellper.CheckStack(context);
-            var left = Expression.Parse(context);
-            ExpressionsHellper.CheckStack(context);
-            var token = context.Tokens.Pop();
-            var rigth = Expression.Parse(context);
-            if (token.TokenString == ">")
-            {
-                return left > rigth;
-            }
-            else if (token.TokenString == "<")
-            {
-                return left < rigth;
-            }
-            else if (token.TokenString == "==")
-            {
-                return left == rigth;
-            }
-            else if (token.TokenString == "!=")
-            {
-                return left != rigth;
-            }
-            else
-                ExpressionsHellper.ThrowUnexpectedToken(token);
-            return false;
         }
 
         Dictionary<string, int> Variables { get; init; } = new();
---
+++ aac5/Statements/ForStatement.cs
@@ -0,0 +1,76 @@
+﻿using Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Interpreter.Interpreter;
+using static Interpreter.Tokenizer;
+
+namespace Interpreter
+{
+    class ForStatement : IStatement
+    {
+        public void Process(Interpreter interpreter, Context context)
+        {
+            ExpressionsHellper.CheckStack(context);
+            var stack = context.Tokens;
+            var charecter = stack.Pop();
+            if (charecter.Type != TokenType.Character)
+                throw ExpressionsHellper.ThrowUnexpectedToken(charecter);
+            ExpressionsHellper.CheckStack(context);
+            var equal = stack.Pop();
+            if (equal.Type != TokenType.Equal)
+                throw ExpressionsHellper.ThrowUnexpectedToken(equal);
+            var left = Expression.Parse(context);
+            ExpressionsHellper.CheckStack(context);
+            var to = stack.Pop();
+            if (to.TokenString != "to")
+                throw ExpressionsHellper.ThrowUnexpectedToken(equal);
+            var right = Expression.Parse(context);
+            context.Variables[charecter.TokenString] = left;
+            EvalForLoop(interpreter, context, charecter, right);
+        }
+
+        private void EvalForLoop(Interpreter interpreter, Context context, Token charecter, int right)
+        {
+            var queue = context.Tokens;
+            var commands = new List<Token>();
+            int bracketCount = 1;
+            var token = queue.Pop();
+            if (token.TokenString != "{")
+                throw ExpressionsHellper.ThrowUnexpectedToken(token);
+            commands.Add(token);
+            while (queue.Count > 0 && bracketCount != 0)
+            {
+                token = queue.Pop();
+                commands.Add(token);
+                if (token.TokenString == "{")
+                    bracketCount++;
+                if (token.TokenString == "}")
+                    bracketCount--;
+            }
+            if (bracketCount > 0)
+                throw new Exception("Expected '}'");
+
+            while (context.Variables[charecter.TokenString] < right)
+            {
+                try
+                {
+                    interpreter.EvalStatement(new Context(commands, context.Variables));
+                    context.Variables[charecter.TokenString]++;
+                }
+                catch (ContinueException)
+                {
+                    context.Variables[charecter.TokenString]++;
+                    continue;
+                }
+                catch (BreakException)
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+}
